854cff1cf6833f512a442967c123f1a9b3ed09c0
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-11-23 08:29:48 -0800

    started signup page

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Styles from './SignUpPage.module.scss';
+import Logo from '../components/images/Decklol.png';
 
 export const SignUpPage = () => {
     return (
-        <div>
-            <h1>Hello I'm the SignUpPage</h1>
-        </div>
+        <>
+            <div className={Styles.InputSec}>
+                <div className={Styles.LogoImage}>
+                    <img src={Logo} alt="Logo"/>    
+                </div>
+
+                <div className={Styles.SignUpTitle}>
+                    <h2>Sign Up</h2>
+                    <div className={Styles.RedLine}></div>
+                    <h3>Sign up with</h3>
+                </div>
+
+                <div className={Styles.InputArea}>
+                    <div className={Styles.Username}>
+                        <h3>Username</h3>
+                        <input type="text" placeholder="Username" className={Styles.UsernameInput} />
+                    </div>
+                    <div className={Styles.Password}>
+                        <h3>Password</h3>
+                        <input type="password" placeholder="Password" className={Styles.UsernameInput} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={Styles.DisplaySec}></div>
+        </>
     )
 }
