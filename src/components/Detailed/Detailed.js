2cfd9f57401044c2be57daac355d66eb7dd8f251
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-04 07:27:09 -0800

    upload fix

--- src/components/Detailed/Detailed.js
+++ src/components/Detailed/Detailed.js
@@ -49,6 +49,9 @@ export const Detailed = ({ user }) => {
                             photoURL: url
                         })
                     }); 
+
+                    setProgress(0);
+                    setImage(null);
             }
         )
     }
@@ -66,8 +69,8 @@ export const Detailed = ({ user }) => {
                         <div className={Styles.ProfileInput}>
                             <div className={Styles.InputStuff}>
                                 
-                                <input type="file" name="file-1[]" id="file-1" className={Styles.inputfile} data-multiple-caption="{count} files selected" multiple onChange={handleChange} />
-                                <label for="file-1">
+                                <input type="file" name="file-2[]" id="file-2" className={Styles.inputfile} data-multiple-caption="{count} files selected" multiple onChange={handleChange} />
+                                <label for="file-2">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"/>
                                     </svg> 
                                     { image ? <span>Ready to Post</span> : <span>Upload An Image</span> }
@@ -75,7 +78,7 @@ export const Detailed = ({ user }) => {
                             </div>
                             <progress className={Styles.Progress} value={progress} max="100" />
                         </div>       
-                                      
+
                         <button onClick={handleUpload}>
                             Upload
                         </button>
