343bf0f12c24894b14f9f079a52f22965a6bd001
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-26 07:47:07 -0800

    stories styling

--- src/components/Feed/Stories.js
+++ src/components/Feed/Stories.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import Styles from './Stories.module.scss';
+import Soto from '../images/juansotolol.jpg';
 
 export const Stories = () => {
     return (
         <div className={Styles.Stories}>
-            <h3>Stories</h3>
+            <div className={Styles.StoriesList}>
+                <div className={Styles.Story}>
+                    <img src={Soto} alt="InstaDeck Story" />
+                    {/* <p>juansoto</p> */}
+                </div>
+
+                <div className={Styles.Story}>
+                    <img src={Soto} alt="InstaDeck Story" />
+                    {/* <p>juansoto</p> */}
+                </div>
+
+                <div className={Styles.Story}>
+                    <img src={Soto} alt="InstaDeck Story" />
+                    {/* <p>juansoto</p> */}
+                </div>
+
+                <div className={Styles.Story}>
+                    <img src={Soto} alt="InstaDeck Story" />
+                    {/* <p>juansoto</p> */}
+                </div>
+            </div>
+
+            <div className={Styles.SettingsList}>
+                <form className={Styles.SearchBox}>
+                    <button><i class="fas fa-search"></i></button>
+                    <input type="text" placeholder="Search" />
+                </form>
+
+                <div className={Styles.Icons}>
+                    <i class="far fa-bell"></i>
+                    <i class="fas fa-cog"></i>
+                    <i class="far fa-moon"></i>
+                </div>
+            </div>
         </div>
     )
 }
