07583f00b80dc28dcb90bb17bc4ef1f40e0a8ba4
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2020-12-30 11:18:08 -0800

    routes helper

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -9,7 +9,7 @@ export const HomePage = ({ user }) => {
             <h1>Welcome to Instadeck</h1>
             <Link to="/signin"><h2>Sign In</h2></Link>
             <Link to="/signup"><h2>Sign Up Here</h2></Link>
-            { user && <Link to="/home"><h2>Home</h2></Link> }
+            <Link to="/home"><h2>Home</h2></Link> 
 
             <h2>By juliuscecilia33</h2>
             <a href="https://github.com/juliuscecilia33">Github</a>
