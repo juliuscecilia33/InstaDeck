337a50b6fcf4183bd4150ed842833c74f7497b18
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-01-03 19:13:26 -0800

    prettier fixes

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,43 +2,49 @@ import React from 'react';
 import Styles from './Feed.module.scss';
 import { Posts } from './Posts';
 // import { Stories } from './Stories'
-import { StoriesContainer, ImageUploadContainer } from '../../containers';
+import {
+  StoriesContainer,
+  ImageUploadContainer,
+} from '../../containers';
 import { PopularPosts } from './PopularPosts';
 
-
 export const Feed = ({ posts, user, popPosts }) => {
+  return (
+    <>
+      <div className={Styles.FeedBg}>
+        <StoriesContainer />
 
-    return (
-        <>
-            
-            <div className={Styles.FeedBg}>
-                <StoriesContainer />
-
-                <div className={Styles.UserFeed}>
-                    
-                    <div className={Styles.FollowingFeed}>
-                        <div className={Styles.Link}>
-                            <h1>Following</h1>
-                        </div>
-
-                        <Posts posts={posts} user={user} />
-                    </div>
-
-                    <div className={Styles.PopularFeed}>
-                        <div className={Styles.Links}>
-                            <a href="/#"><h3>Popular</h3></a>
-                            <a href="/#"><h3>Notifications</h3></a>
-                            <a href="/#"><h3>Messages</h3></a>
-                            <a href="/#"><h3>Saved</h3></a>
-                        </div>
-
-                        <PopularPosts posts={posts} user={user} />
-                    </div>
-                </div>
-
-                
-                <ImageUploadContainer user={user} />
+        <div className={Styles.UserFeed}>
+          <div className={Styles.FollowingFeed}>
+            <div className={Styles.Link}>
+              <h1>Following</h1>
             </div>
-        </>
-    )
-}
+
+            <Posts posts={posts} user={user} />
+          </div>
+
+          <div className={Styles.PopularFeed}>
+            <div className={Styles.Links}>
+              <a href="/#">
+                <h3>Popular</h3>
+              </a>
+              <a href="/#">
+                <h3>Notifications</h3>
+              </a>
+              <a href="/#">
+                <h3>Messages</h3>
+              </a>
+              <a href="/#">
+                <h3>Saved</h3>
+              </a>
+            </div>
+
+            <PopularPosts posts={posts} user={user} />
+          </div>
+        </div>
+
+        <ImageUploadContainer user={user} />
+      </div>
+    </>
+  );
+};
--- src/containers/imageUpload.js
+++ src/containers/imageUpload.js
@@ -1,72 +1,80 @@
 import React, { useState } from 'react';
 import { ImageUpload } from '../components';
 import { storage, db } from '../firebase';
-import firebase from "firebase";
+import firebase from 'firebase';
 
 export function ImageUploadContainer({ user }) {
-    const [image, setImage] = useState(null);
-    const [progress, setProgress] = useState(0);
-    const [caption, setCaption] = useState('');
+  const [image, setImage] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const [caption, setCaption] = useState('');
 
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0]);
-        }
+  const handleChange = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
     }
+  };
 
-    const handleUpload = () => {
-        const uploadTask = storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-                // progress function ... 
-                const progress = Math.round(
-                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-                );
-                setProgress(progress);
-            },
-            (error) => {
-                // Error function...
-                alert(error.message);
-            },
-            () => {
-                // when the upload completes...
-                storage
-                    .ref("images")
-                    .child(image.name)
-                    .getDownloadURL() // thet image is already uploaded, this gives us a download link for the uploaded image
-                    .then(url => {
-                        // post image inside database
-                        db.collection("posts").add({
-                            timestamp: firebase.firestore.FieldValue.serverTimestamp(), // allows the most recent image to be on top
-                            caption: caption,
-                            imageUrl: url,
-                            username: user.displayName,
-                            usernamepic: user.photoURL,
-                            likes: 0,
-                            comments: 0,
-                        });
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${image.name}`).put(image);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        // progress function ...
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100,
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        // Error function...
+        alert(error.message);
+      },
+      () => {
+        // when the upload completes...
+        storage
+          .ref('images')
+          .child(image.name)
+          .getDownloadURL() // thet image is already uploaded, this gives us a download link for the uploaded image
+          .then((url) => {
+            // post image inside database
+            db.collection('posts').add({
+              timestamp: firebase.firestore.FieldValue.serverTimestamp(), // allows the most recent image to be on top
+              caption: caption,
+              imageUrl: url,
+              username: user.displayName,
+              usernamepic: user.photoURL,
+              likes: 0,
+              comments: 0,
+            });
 
-                        setProgress(0);
-                        setCaption("");
-                        setImage(null);
-                    });
+            setProgress(0);
+            setCaption('');
+            setImage(null);
+          });
+      },
+    );
+  };
 
-                    
-            }
-        )
-    }
-
-    return (
-        <ImageUpload>
-            <ImageUpload.Contain>
-                <ImageUpload.InputStuff handleChange={handleChange}>
-                    { image ? <span>Ready to Post</span> : <span>Upload An Image</span> }
-                </ImageUpload.InputStuff>
-                <ImageUpload.CaptionInput setCaption={setCaption} caption={caption} />
-                <ImageUpload.Progress progress={progress} />
-                <ImageUpload.PostButton handleUpload={handleUpload} image={image} />
-            </ImageUpload.Contain>
-        </ImageUpload>
-    )
+  return (
+    <ImageUpload>
+      <ImageUpload.Contain>
+        <ImageUpload.InputStuff handleChange={handleChange}>
+          {image ? (
+            <span>Ready to Post</span>
+          ) : (
+            <span>Upload An Image</span>
+          )}
+        </ImageUpload.InputStuff>
+        <ImageUpload.CaptionInput
+          setCaption={setCaption}
+          caption={caption}
+        />
+        <ImageUpload.Progress progress={progress} />
+        <ImageUpload.PostButton
+          handleUpload={handleUpload}
+          image={image}
+        />
+      </ImageUpload.Contain>
+    </ImageUpload>
+  );
 }
