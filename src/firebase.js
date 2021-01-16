70261861f3c1fe8139d00ea3b712173c4a8e1390
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-01-16 09:37:11 -0800

    removed admin control

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,4 @@
 import firebase from "firebase";
-import * as admin from "firebase-admin";
-var serviceAccount = require("C:/Users/Julius/Desktop/instadeck-c9606-firebase-adminsdk-qj3qn-9088cb83a7.json");
 
 const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,13 +10,8 @@ const firebaseApp = firebase.initializeApp({
   appId: "1:793431210337:web:9814b1df12575fdeaf6d20",
 });
 
-const adminControl = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://instadeck-c9606.firebaseio.com",
-});
-
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
-export { firebaseApp, db, auth, storage, adminControl };
+export { firebaseApp, db, auth, storage };
